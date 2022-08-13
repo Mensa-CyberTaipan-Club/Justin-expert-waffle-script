a0bf6720dc073a54c509c7f9524c8eff7c989613
DragonMasterAndCat <108171705+DragonMasterAndCat@users.noreply.github.com>
2022-08-13 17:00:34 +0800

    Update wizard.sh

--- wizard.sh
+++ wizard.sh
@@ -1,6 +1,7 @@
 echo "Preparing to use the weave..."
 sleep 1
 echo "Reviewing spellbook..."
+
 sleep 1
 echo "Memorizing verbal components..."
 sleep 1
@@ -8,4 +9,6 @@ echo "Practicing somatic components..."
 sleep 1
 echo "collecting material components..."
 sleep 1
-echo "Casting fireball..."
+echo "What spell would you like to cast?"
+read varspell
+echo "Casting $varspell..."
