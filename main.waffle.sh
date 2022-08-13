fcafc0c560741455aeea53347293b7290d3c9c50
DragonMasterAndCat <108171705+DragonMasterAndCat@users.noreply.github.com>
2022-08-13 16:28:24 +0800

    Update main.waffle.sh

--- main.waffle.sh
+++ main.waffle.sh
@@ -1,11 +1,11 @@
 echo "Preparing to use the weave..."
-sleep .5
+sleep 5
 echo "Reviewing spellbook..."
-sleep .5
+sleep 5
 echo "Memorizing verbal components..."
-sleep .5
+sleep 5
 echo "Practicing somatic components..."
-sleep .5
+sleep 5
 echo "collecting material components..."
-sleep .5
+sleep 5
 echo "Casting fireball..."
